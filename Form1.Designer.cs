5516f81f33dfab18c2efac8fbfea1c656430d6b0
isa <isaturk07@gmail.com>
2019-03-06 23:52:00 +0300

    First Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace YatiliKronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(596, 370);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat/Duraklat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sıfırla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(476, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Designed by İsa Türk";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(269, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Focus";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 370);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Koç Lisesi Bilgi Yarışması";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace YatiliKronometre
+
+{
+
+
+    
+    public partial class Form1 : Form
+    {
+
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        int sn =00,mm=00,hr = 00;
+        string snfm, mmfm, hrfm;
+        int ms;
+        bool runnnin = false,focused = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            int FirstHotkeyId = 1;
+            // Set the Hotkey triggerer the F9 key 
+            // Expected an integer value for F9: 0x78, but you can convert the Keys.KEY to its int value
+            // See: https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+            int FirstHotKeyKey = (int)Keys.K;
+            // Register the "F9" hotkey
+            Boolean F9Registered = RegisterHotKey(
+                this.Handle, FirstHotkeyId, 0x0000, FirstHotKeyKey
+            );
+
+            // Repeat the same process but with F10
+            int SecondHotkeyId = 2;
+            int SecondHotKeyKey = (int)Keys.L;
+            Boolean F10Registered = RegisterHotKey(
+                this.Handle, SecondHotkeyId, 0x0000, SecondHotKeyKey
+            );
+
+            // 4. Verify if both hotkeys were succesfully registered, if not, show message in the console
+            if (!F9Registered)
+            {
+
+                Console.WriteLine("Global Hotkey F9 couldn't be registered !");
+            }
+
+            if (!F10Registered)
+            {
+                Console.WriteLine("Global Hotkey F10 couldn't be registered !");
+            }
+
+        }
+
+
+
+ 
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TransparencyKey = Color.Turquoise;
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label1.Top = (this.ClientSize.Height - label1.Height) / 2;
+            label1.Text = "00:00:00";
+            this.KeyPreview = true;
+            
+            
+
+        }
+        protected override void WndProc(ref Message m)
+        {
+            // 5. Catch when a HotKey is pressed !
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                // MessageBox.Show(string.Format("Hotkey #{0} pressed", id));
+
+                // 6. Handle what will happen once a respective hotkey is pressed
+                switch (id)
+                {
+                    case 1:
+                        timer1.Stop();
+                        runnnin = false;
+                        sn = 00;
+                        mm = 00;
+                        hr = 00;
+                        label1.Text = "00:00:00";
+                        break;
+                    case 2:
+                        if (runnnin == false)
+                        {
+                            timer1.Start();
+                            runnnin = true;
+
+                        }
+                        else
+                        {
+                            timer1.Stop();
+                            runnnin = false;
+                        }
+
+                        break;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            runnnin = false;
+            sn = 00;
+            mm = 00;
+            hr = 00;
+            label1.Text = "00:00:00";
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (focused == false)
+            {
+                this.BackColor = Color.Turquoise;
+                this.FormBorderStyle = FormBorderStyle.None;
+                button1.Visible = false;
+                button2.Visible = false;
+                pictureBox1.Visible = false;
+                button3.Text = "Unfocus";
+                focused = true;
+            }
+            else
+            {
+                this.BackColor = SystemColors.Control;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                button1.Visible = true;
+                button2.Visible = true;
+                pictureBox1.Visible = true;
+                button3.Text = "Focus";
+                focused = false;
+            }
+            
+
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (runnnin == false)
+            {
+                timer1.Start();
+                runnnin = true;
+
+            }
+            else
+            {
+                timer1.Stop();
+                runnnin = false;
+            }
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            increase();
+            reformat();
+
+            
+        }
+
+
+        public void reformat()
+        {
+
+            if (sn <10 || mm<10|| hr <10)
+            {
+
+                if (hr<10)
+                {
+                    hrfm = "0" + hr;
+                }
+                else { hrfm = hr.ToString(); }
+                if (mm <10)
+                {
+                    mmfm = "0" + mm;
+                }
+                else { mmfm = mm.ToString(); }
+                if (sn<10)
+                {
+                    snfm = "0" + sn;
+                }
+                else { snfm = sn.ToString(); }
+
+                label1.Text = hrfm + ":" + mmfm + ":" + snfm;
+
+
+            }
+            else
+            {
+                label1.Text = hr + ":" + mm + ":" + sn;
+            }
+           
+
+
+        }
+        public void increase()
+        {
+            if (sn != 59)
+            {
+                sn++;
+
+            }
+            else
+            {
+                if (mm !=59)
+                {
+                    sn = 00;
+                    mm++;
+                }
+                else
+                {
+                    mm = 00;
+                    sn = 00;
+                    hr++;
+                }
+            }
+        }
+    }
+}
